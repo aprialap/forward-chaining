1a5998b22801ee05be67bc2c066afa4dea555d09
aprialap <aprial.aap@gmail.com>
2020-05-19 22:56:52 +0700

    update cms login admin

--- resources/views/umum/login-admin.blade.php
+++ resources/views/umum/login-admin.blade.php
@@ -16,12 +16,33 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h4 class="card-title">Login Admin</h4>
-                        
+                
+    <div class="login-form-bg h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100">
+                <div class="col-xl-6">
+                    <div class="form-input-content">
+                        <div class="card login-form mb-0">
+                            <div class="card-body pt-5">
+                                <a class="text-center" href="#"> <h4>Login Admin</h4></a>
+        
+                                <form class="mt-5 mb-5 login-input">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" name="username" placeholder="username">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control" name="password" placeholder="Password">
+                                    </div>
+                                    <button class="btn login-form__btn submit w-100">Login</button>
+                                </form>
+                               
+                            </div>
+                        </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
             </div>
         </div>
     </div>
